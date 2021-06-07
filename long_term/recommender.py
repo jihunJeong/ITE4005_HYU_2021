bd6f253e94b5313fb4f949ea24c6c7058543d9df
jihunJeong <jihun.jeong219@gmail.com>
2021-06-07 10:20:06 +0900

    Update MF Code

--- long_term/recommender.py
+++ long_term/recommender.py
@@ -5,18 +5,31 @@ import math
 import torch
 from torch.autograd import Variable
 
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
 class MatrixFactorization(torch.nn.Module):
     def __init__(self, n_users, n_items, n_factors=20):
         super().__init__()
-	# create user embeddings
         self.user_factors = torch.nn.Embedding(n_users, n_factors,
                                                sparse=True)
-	# create item embeddings
         self.item_factors = torch.nn.Embedding(n_items, n_factors,
                                                sparse=True)
 
     def forward(self, user, item):
-    	# matrix multiplication
         return (self.user_factors(user)*self.item_factors(item)).sum(1)
 
     def predict(self, user, item):
@@ -31,27 +44,54 @@ if __name__ == "__main__":
     df_ratings = pd.read_csv("./data-2/"+train, sep='\s+', names=['user','movie','rating','timestamp'])
     df_ratings.drop('timestamp', axis=1, inplace=True)
     df_test = pd.read_csv("./data-2/"+test, sep='\s+', names=['user','movie','rating','timestamp'])
-    
+    print("Done")
+
+    print("Matrix Factorization ... ", end="", flush=True)
     n_users = max(df_ratings.user.unique())
-    n_items = max(df_ratings.movie.unique())
+    n_movies = max(df_ratings.movie.unique())
     
-    model = MatrixFactorization(n_users, n_items, n_factors=20)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model = MatrixFactorization(n_users, n_movies, n_factors=10)
+    model.to(device)
     loss_fn = torch.nn.MSELoss() 
-    optimizer = torch.optim.SGD(model.parameters(),
-                                lr=1e-6)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-5)
 
-    for user, item in zip(users, items):
-        # get user, item and rating data
-        rating = Variable(torch.FloatTensor([ratings[user, item]]))
-        user = Variable(torch.LongTensor([int(user)]))
-        item = Variable(torch.LongTensor([int(item)]))
+    ratings = np.zeros((n_users+1, n_movies+1)) 
+    for row in df_ratings.itertuples(index=False):
+        user_id, movie_id, _ = row
+        ratings[user_id, movie_id] = row[2]
+    
+    train_loss = AverageMeter()
+    users = df_ratings['user']
+    movies = df_ratings['movie']
+    for idx, (user, movie) in enumerate(zip(users, movies)):
+        optimizer.zero_grad()
 
-        # predict
-        prediction = model(user, item)
+        rating = Variable(torch.FloatTensor([ratings[user, movie]])).to(device)
+        user = Variable(torch.LongTensor([int(user)])).to(device)
+        movie = Variable(torch.LongTensor([int(movie)])).to(device)
+
+        prediction = model(user, movie)
         loss = loss_fn(prediction, rating)
-
-        # backpropagate
         loss.backward()
-
-        # update weights
         optimizer.step()
+
+        train_loss.update(loss.item(), 1)
+        if (idx+1) % 1000 == 0:
+            print("Index %d | Train Loss %.4f" % (idx+1, train_loss.avg))
+
+    print("Done")
+
+    print("Test ... ", end="", flush=True)
+    result_df = df_test.drop(['timestamp'], axis=1).copy()
+    result_df.astype({'user':'int','movie':'int','rating':'float'})
+    for idx, row in result_df.iterrows():
+        user = Variable(torch.LongTensor([int(row['user'])]))
+        movie = Variable(torch.LongTensor([int(row['movie'])]))
+        if user > n_users or movie > n_movies:
+            result_df.loc[idx, 'rating'] = sum(rating[int(row['user'])]) / n_movies
+        else :
+            result_df.loc[idx,'rating'] = float(model(user, movie))
+     
+    result_df.to_csv('./test/'+train[:2]+".base_prediction.txt", sep='\t', index=False, header=False)
+    print("Done")
